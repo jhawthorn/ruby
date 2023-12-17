5c9f48a5b104090cfcf77e89d21f0a7b7289f27a
John Hawthorn <john@hawthorn.email>
2023-12-16 22:06:59 -0800

    Remove clear of stack

--- vm.c
+++ vm.c
@@ -2994,7 +2994,6 @@ ruby_vm_destruct(rb_vm_t *vm)
         rb_thread_t *th = vm->ractor.main_thread;
         rb_ractor_set_current_ec(vm->ractor.main_ractor, NULL);
 
-        VALUE *stack = th->ec->vm_stack;
         if (rb_free_on_exit) {
             rb_free_default_rand_key();
             rb_free_encoded_insn_data();
@@ -3058,10 +3057,6 @@ ruby_vm_destruct(rb_vm_t *vm)
             if (rb_free_on_exit) {
                 rb_objspace_free_objects(objspace);
                 rb_free_generic_iv_tbl_();
-                if (th) {
-                    xfree(stack);
-                    ruby_mimfree(th);
-                }
             }
             rb_objspace_free(objspace);
         }
