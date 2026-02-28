07fd56280df86cb1fe8ac4ca891a2b86bbd71310
John Hawthorn <john@hawthorn.email>
2026-02-27 23:26:46 -0800

    Batch cleanup id2ref and geniv before sweep
    Previously we needed to check every object to determine whether or not
    it might have an entry in the id2ref table or geniv table. Both of these
    should now be uncommon.
    
    This commit adds a function, rb_gc_vm_weak_table_essential_p, which
    allows gc.c to decide which tables to always handle in batch.
    
    MMTK will continue to call rb_gc_vm_weak_table_foreach on all tables.
    This only changes the default GC to sweep the "essential" tables first,
    but continue to call rb_gc_obj_free_vm_weak_references on other objects.
    
    This makes the id2ref and geniv table "essental", as they're small and
    need to be tested on every type of object. It leaves the fstring table,
    symbol table, callinfo table, and overloaded CME table as-is because
    those tables are (mostly) larger and are only needed by specific object
    types.

--- gc/default/default.c
+++ gc/default/default.c
@@ -3745,6 +3745,15 @@ gc_ractor_newobj_cache_clear(void *c, void *data)
     }
 }
 
+static int
+gc_sweep_weak_table_i(VALUE val, void *data)
+{
+    rb_objspace_t *objspace = data;
+    if (RB_SPECIAL_CONST_P(val)) return ST_CONTINUE;
+    if (RVALUE_MARKED(objspace, val)) return ST_CONTINUE;
+    return ST_DELETE;
+}
+
 static void
 gc_sweep_freeobj_hooks(rb_objspace_t *objspace)
 {
@@ -3797,6 +3806,17 @@ gc_sweep_start(rb_objspace_t *objspace)
 
     if (RB_UNLIKELY(objspace->hook_events & RUBY_INTERNAL_EVENT_FREEOBJ)) {
         gc_sweep_freeobj_hooks(objspace);
+    }
+
+    for (int table = 0; table < RB_GC_VM_WEAK_TABLE_COUNT; table++) {
+        if (!rb_gc_vm_weak_table_essential_p(table)) continue;
+        rb_gc_vm_weak_table_foreach(
+            gc_sweep_weak_table_i,
+            NULL,
+            objspace,
+            true,
+            table
+        );
     }
 
 #if GC_CAN_COMPILE_COMPACTION
